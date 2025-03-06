69943a4b1efa4f3da3ef892ea404a93ef67dfe8a
Jeon-man <jhmjhm0106@naver.com>
2025-03-06 16:57:03 +0900

    FEAT: Add floating toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import { ModeToggle } from "@/components/common/theme-toggle";
 import { CreateSurveyForm } from "@/components/survey-form";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center p-72">
-      <CreateSurveyForm />
+    <div className="">
+      <div className="fixed top-4 right-4 z-50">
+        <ModeToggle />
+      </div>
+      <div className="flex justify-center items-center p-72">
+        <CreateSurveyForm />
+      </div>
     </div>
   );
 }
