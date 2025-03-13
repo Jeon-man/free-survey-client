305edecb676b56aa526bac0d591c8674d75ddee4
Jeon-man <jhmjhm0106@naver.com>
2025-03-13 17:08:50 +0900

    REFACTOR: Remove `Form`

--- src/components/survey-form.tsx
+++ src/components/survey-form.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-
 import {
   AlertDialog,
   AlertDialogAction,
@@ -22,34 +18,18 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
-
-const formSchema = z.object({
-  title: z.string().min(2, {
-    message: "title must be at least 2 characters.",
-  }),
-});
+import { useState } from "react";
 
 export function CreateSurveyForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: "",
-    },
-  });
+  const [title, setTitle] = useState<string>("");
+  const [open, setOpen] = useState<boolean>(false);
 
-  function onCreateSurvey(values: z.infer<typeof formSchema>) {
-    // send on create survey api
-    console.log(values);
+  function onCreateSurvey() {
+    if (title.length <= 2) {
+      alert("Title must be at least 2 characters long.");
+    }
   }
 
   return (
@@ -57,53 +37,40 @@ export function CreateSurveyForm() {
       <CardHeader>
         <CardTitle>Create Survey</CardTitle>
       </CardHeader>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onCreateSurvey)}
-          className="space-y-4 md:space-y-8"
-        >
-          <CardContent>
-            <FormField
-              control={form.control}
-              name="title"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Title</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="2025 Preference research"
-                      {...field}
-                      className="w-full"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </CardContent>
-          <CardFooter className="flex justify-end">
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <Button className="w-full md:w-auto">Create Survey</Button>
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>{`Do you want to make it ${form.getValues(
-                    "title"
-                  )}?`}</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    When you create a URL, you need to save it somewhere.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel>Cancel</AlertDialogCancel>
-                  <AlertDialogAction type="submit">Create</AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          </CardFooter>
-        </form>
-      </Form>
+      <CardContent>
+        <Input
+          placeholder="title"
+          onChange={(event) => setTitle(event.target.value)}
+        />
+      </CardContent>
+      <CardFooter className="flex justify-end">
+        <AlertDialog open={open} onOpenChange={setOpen}>
+          <AlertDialogTrigger
+            asChild
+            onClick={() => {
+              if (title.length >= 2) {
+                setOpen(true);
+              }
+            }}
+          >
+            <Button className="w-full md:w-auto">Create Survey</Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>{`Do you want to make it ${title}?`}</AlertDialogTitle>
+              <AlertDialogDescription>
+                When you create a URL, you need to save it somewhere.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={onCreateSurvey}>
+                Create
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </CardFooter>
     </Card>
   );
 }
